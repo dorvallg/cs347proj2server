98eca6f8dd3a3791e23431059722ede4f02702c3
Christopher Walton <walto2cj@dukes.jmu.edu>
2020-12-08 16:17:07 -0500

    bet-table.sql fixes

--- server/bet-table.sql
+++ server/bet-table.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS bet;
 CREATE TABLE bet(
   id SERIAL PRIMARY KEY,
   odds INT,
-  is_happening INT;
+  is_happening INT,
   betName TEXT,
   expires_at TIMESTAMP,
   is_expired INT
